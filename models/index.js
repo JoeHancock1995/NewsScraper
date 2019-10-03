bcbb58341e493d6f1c59745d857d3f1096db78a2
Joe Hancock <joe.hancock95@gmail.com>
2019-10-03 01:39:02 -0500

    added server info

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Note: require("./Articles"),
+    Note: require("./Comments"),
+    User: require("./User")
+  };
+  
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
+var handlebars = require("handlebars");
+var mongoose = require("mongoose")
 
-// Make a request via axios to grab the HTML body from the site of your choice
+
+mongoose.connect("mongodb://localhost/newsscraperdb", { useNewUrlParser: true });
+
+//  ========cheerio scraping my website========
+// Make a request via axios to grab the HTML body from the site
 axios.get("https://www.kxan.com").then(function(response) {
-
   // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  // '$' becomes a shorthand for cheerio's selector commandslike jQuery's '$'
   var $ = cheerio.load(response.data);
-
   // An empty array to save the data that we'll scrape
   var results = [];
-
   // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
   $("article").each(function(i, element) {
-
     var title = $(element).children().text();
     var link = $(element).find("a").attr("href");
     var summary = $(element).children().text();
     var img = $(element).find("img").attr("src");
-        // Save these results in an object that we'll push into the results array we defined earlier
+        // Save these results in an object, then into the results array
     results.push({
       title: title,
       link: link,
@@ -28,7 +28,6 @@ axios.get("https://www.kxan.com").then(function(response) {
       img: img
     });
   });
-
   // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
 });
