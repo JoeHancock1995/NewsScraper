e7690bf8cbe5b96badbf823bb578eb339a34daf1
Joe Hancock <joe.hancock95@gmail.com>
2019-10-05 16:41:05 -0500

    app.js has well written psuedocode upon better understanding of how it works

---
+++ public/app.js
@@ -0,0 +1,13 @@
+// ----------things to put in app.js
+// 1. function to loop through articles.
+// 2. build a div card for each one after looping each article
+// 3. make buttons work- fetch articles, add/ delete comments, etc
+// 4. append card and info
+// 5. similar fashion as articles but have a loop go over comments
+// 6. create a way for these comments to be edited, create any buttons and add to car
+//    (maybe reference the mysql todolist for syntax?)
+// 7. make a request to take in changes made by user. WIll require some form of validation.
+// 8. append the card to articles div
+// 9. call my scraper function route from server.js and console log
+// 10. make Scraper buttons in nav methods for onclick events. Make sure it doesnt delete anything on screen.
+// 11. if anything is edited pages should reload itself upon return to reflect comments and update scrae
