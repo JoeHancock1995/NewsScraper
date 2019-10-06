356a0f7d59b7592a619fe0e22c9e752e64eadda1
Joe Hancock <joe.hancock95@gmail.com>
2019-10-06 00:51:05 -0500

    changed comment js back to note js to follow old activities for better understanding. App.js and populate then done

--- models/User.js
+++ models/User.js
@@ -24,9 +24,7 @@ var UserSchema = new Schema({
     }
   ]
 });
-
 // This creates our model from the above schema, using mongoose's model method
 var User = mongoose.model("User", UserSchema);
-
 // Export the User model
 module.exports = User;
