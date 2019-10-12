d13e8f36a7793d4c146180b663aa9a4d6754a1c3
Joe Hancock <joe.hancock95@gmail.com>
2019-10-12 17:53:31 -0500

    added a button which will be for comments on card. no styling

--- public/app.js
+++ public/app.js
@@ -26,12 +26,50 @@ function getArticles(){
       var p =$('<p>').text(dbArticle[i].link[0]);
       p.addClass('byLine');
       p.html(dbArticle[i].byLine);
-      cardBody.append(cardHeader,p);
+      var btnComments=$('<button>');
+      btnComments.addClass('comment-button');
+      // btnComments.attr('id', data[i]._id);
+
+      cardBody.append(cardHeader,p, btnComments);
       card.append(cardBody);
       $('#articles').append(card);
     }
   });
 };
+
+// // Whenever someone clicks a p tag
+// $(document).on("click", "card", function() {
+//   // Empty the notes from the note section
+//   $("#notes").empty();
+//   // Save the id from the p tag
+//   var thisId = $(this).attr("data-id");
+
+//   // Now make an ajax call for the Article
+//   $.ajax({
+//     method: "GET",
+//     url: "/articles/" + thisId
+//   })
+//     // With that done, add the note information to the page
+//     .then(function(data) {
+//       console.log(data);
+//       // The title of the article
+//       $("#notes").append("<h2>" + data.title + "</h2>");
+//       // An input to enter a new title
+//       $("#notes").append("<input id='titleinput' name='title' >");
+//       // A textarea to add a new note body
+//       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+//       // A button to submit a new note, with the id of the article saved to it
+//       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+//       // If there's a note in the article
+//       if (data.note) {
+//         // Place the title of the note in the title input
+//         $("#titleinput").val(data.note.title);
+//         // Place the body of the note in the body textarea
+//         $("#bodyinput").val(data.note.body);
+//       }
+//     });
+// });
 
 // 2. build a div card for each one after looping each article
 // 3. make buttons work- fetch articles, add/ delete comments, etc
